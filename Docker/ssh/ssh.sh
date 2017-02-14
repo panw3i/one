b17c268dfd1a67eac431ef6e7425865e3e1b9635
JIOBXN <jiobxn@gmail.com>
2017-02-14 22:50:41 +0800

    Update ssh.sh

--- Docker/ssh/ssh.sh
+++ Docker/ssh/ssh.sh
@@ -7,7 +7,8 @@ set -e
 __setup(){
     mkdir -p /var/run/sshd
 	echo "$USER:$PASS" | chpasswd
-	ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' 
+	ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+	sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 	\cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 }
 
