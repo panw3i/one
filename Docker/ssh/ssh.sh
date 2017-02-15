0c6d8f0a7f67986fbec1722a2a0454e5eb0fa6ed
JIOBXN <jiobxn@gmail.com>
2017-02-15 15:18:14 +0800

    Update ssh.sh

--- Docker/ssh/ssh.sh
+++ Docker/ssh/ssh.sh
@@ -5,7 +5,7 @@ set -e
 : ${PASS:=$(pwmake 64)}
 
 __setup(){
-    mkdir -p /var/run/sshd
+	mkdir -p /var/run/sshd
 	echo "$USER:$PASS" | chpasswd
 	ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
 	sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
