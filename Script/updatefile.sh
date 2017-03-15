a9bc9e9dfab488a5df3b2516eccb2bc0849bdce1
JIOBXN <jiobxn@gmail.com>
2017-03-15 17:27:32 +0800

    Update updatefile.sh

--- Script/updatefile.sh
+++ Script/updatefile.sh
@@ -13,7 +13,7 @@ done
 for i in $(find $dir -type f); do
         if [ -f "$www/$i" ]; then
 		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[32m Update File \033[0m"
-		[ "$1" == "yes" ] &&  cp "$www/$i" "$www/$i-$(date +%F)" && \cp $i "$www/$i"
+		[ "$1" == "yes" ] &&  cp "$www/$i" "$www/$i-$(date +%F_%T)" && \cp $i "$www/$i"
         else
 		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Update File \033[0m"
 		[ "$1" == "yes" ] &&  \cp $i "$www/$i"
