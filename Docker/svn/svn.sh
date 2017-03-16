beed2f44f75de5d8904e5e8fc0ce22efc5dc2dc1
JIOBXN <jiobxn@gmail.com>
2017-03-16 17:33:18 +0800

    Create svn.sh

---
+++ Docker/svn/svn.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'httpd' ]; then
+
+: ${ADMIN:=admin}
+: ${USER:=user1}
+: ${ADMIN_PASS:=passwd0}
+: ${USER_PASS:=passwd1}
+
+if [ -z "$(grep "redhat.xyz" /etc/httpd/conf/httpd.conf)" ]; then
+	echo "Initialize httpd"
+	#key
+	if [ -f /key/server.crt -a -f /key/server.key ]; then
+		\cp /key/server.crt /etc/pki/tls/certs/localhost.crt
+		\cp /key/server.key /etc/pki/tls/private/localhost.key
+	fi
+	
+	#httpd
+	cat >>/etc/httpd/conf/httpd.conf <<-END
+	#
+	#redhat.xyz
+	ServerName localhost
+	AddDefaultCharset UTF-8
+
+	<IfModule deflate_module>  
+	AddOutputFilterByType DEFLATE all  
+	SetOutputFilter DEFLATE  
+	</ifModule>  
+
+	#svn
+	<Location /svn>
+	  DAV svn
+	  SVNParentPath /home/svn
+	  SVNListParentPath on
+
+	  # Authentication: Basic
+	  AuthName "Subversion repository"
+	  AuthType Basic
+	  AuthBasicProvider file
+	  AuthUserFile /home/svn/conf/htpasswd
+	  Require valid-user
+
+	  # Authorization: Path-based access control
+	  AuthzSVNAccessFile /home/svn/conf/authz
+	</Location>
+	END
+
+	#svn
+	cat >/home/svnserve.conf.txt<<-END
+	[general]
+	anon-access = none
+	#有效值是"write", "read",and "none".
+	auth-access = write
+	password-db = /home/svn/conf/passwd
+	authz-db = /home/svn/conf/authz
+	realm = /home/svn
+	END
+
+	cat >/home/authz.txt<<-END
+	[groups]
+	manager=admin
+	users=user1,user2
+
+	[/]
+	@manager=rw
+	*=
+
+	[repos:/]
+	@manager=rw
+	@users=rw
+	*=
+	END
+
+	if [ ! -d /home/svn/conf ]; then
+		echo "create default repository"
+		mkdir -p /home/svn/conf
+		[ ! -d /home/svn/repos ] && svnadmin create /home/svn/repos
+
+		echo "$ADMIN:$(openssl passwd -apr1 $ADMIN_PASS)" > /home/svn/conf/htpasswd
+		echo "$USER:$(openssl passwd -apr1 $USER_PASS)" >> /home/svn/conf/htpasswd
+		echo -e "$ADMIN = $ADMIN_PASS\n$USER = $USER_PASS" |tee >/home/svn/conf/passwd
+
+		\cp /home/svnserve.conf.txt /home/svn/conf/svnserve.conf
+		\cp /home/authz.txt /home/svn/conf/authz
+	else
+		if [ -f /home/svn/conf/authz ]; then 
+			echo "authz exist"
+			for i in $(grep : /home/svn/conf/authz |grep -Po '(?<=\[)[^)]*(?=\])' |awk -F: '{print $1}'); do
+				[ ! -d "/home/svn/$i" ] && svnadmin create /home/svn/$i && echo "create $i repository"
+			done
+		else
+			\cp /home/authz.txt /home/svn/conf/authz
+			[ ! -d /home/svn/repos ] && svnadmin create /home/svn/repos && echo "create default repository"
+		fi
+	
+		if [ -f /home/svn/conf/passwd ]; then
+			echo "passwd exist"
+			\rm /home/svn/conf/htpasswd 2>/dev/null || echo
+			for i in $(sed 's/ //g' /home/svn/conf/passwd); do
+				user=$(echo $i |awk -F= '{print $1}')
+				pass=$(echo $i |awk -F= '{print $2}')
+				echo "$user:$(openssl passwd -apr1 $pass)" >> /home/svn/conf/htpasswd && echo "$user = $pass"
+			done
+		else
+			echo "$ADMIN:$(openssl passwd -apr1 $ADMIN_PASS)" > /home/svn/conf/htpasswd
+			echo "$USER:$(openssl passwd -apr1 $USER_PASS)" >> /home/svn/conf/htpasswd
+			echo -e "$ADMIN = $ADMIN_PASS\n$USER = $USER_PASS" |tee >/home/svn/conf/passwd
+		fi
+		\cp /home/svnserve.conf.txt /home/svn/conf/svnserve.conf
+	fi
+fi
+	echo "Start ****"
+	svnserve -d -r /home/svn
+	exec "$@"
+else
+
+    echo -e "
+	Example:
+				docker run -d \\
+				-v /docker/svn:/home/svn \\
+				-v /docker/key:/key \\
+				-p 10080:80 \\
+				-p 10443:443 \\
+        -p 13690:3690 \\
+				-e ADMIN=[admin] \\
+				-e USER=[user1] \\
+				-e ADMIN_PASS=[passwd0] \\
+				-e USER_PASS=[passwd1] \\
+				--hostname svn \\
+				--name svn svn
+
+	Or prepare /docker/svn/conf/authz and /docker/svn/conf/passwd files.
+	"
+fi
