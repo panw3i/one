b82040497319c51d9e948385c947c773bb0bbf79
JIOBXN <jiobxn@gmail.com>
2017-03-20 14:33:44 +0800

    Update updatefile.sh

--- Script/updatefile.sh
+++ Script/updatefile.sh
@@ -5,7 +5,7 @@ dir="h5"
 
 for i in $(find $dir -type d); do
 	if [ ! -d "$www/$i" ]; then
-		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[32m Add Directory \033[0m"
+		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Add Directory \033[0m"
 		[ "$1" == "yes" ] &&  mkdir "$www/$i"
 	fi
 done
@@ -15,7 +15,7 @@ for i in $(find $dir -type f); do
 		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[32m Update File \033[0m"
 		[ "$1" == "yes" ] &&  cp "$www/$i" "$www/$i-$(date +%F_%T)" && \cp $i "$www/$i"
         else
-		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Update File \033[0m"
+		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Add File \033[0m"
 		[ "$1" == "yes" ] &&  \cp $i "$www/$i"
         fi
 done
