0195c5742573dc543e79033a7b3ea5e68123e59a
JIOBXN <jiobxn@gmail.com>
2017-03-30 17:02:28 +0800

    Update updatefile.sh

--- Script/updatefile.sh
+++ Script/updatefile.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-www="/var/www/html"
-dir="h5"
+WWW="/var/www/html"
+DIR="h5"
+TIME="$(date +%F_%T)"
 
-for i in $(find $dir -type d); do
-	if [ ! -d "$www/$i" ]; then
-		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Add Directory \033[0m"
-		[ "$1" == "yes" ] &&  mkdir "$www/$i"
+for i in $(find $DIR -type d); do
+	if [ ! -d "$WWW/$i" ]; then
+		[ "$1" == "L" ] && echo -e "$WWW/$i  \033[32m Add Directory \033[0m"
+		[ "$1" == "yes" ] && mkdir "$WWW/$i"
+#		[ "$1" == "yes" ] && mkdir -p "$TIME/$i"
 	fi
 done
 
-for i in $(find $dir -type f); do
-        if [ -f "$www/$i" ]; then
-		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[32m Update File \033[0m"
-		[ "$1" == "yes" ] &&  cp "$www/$i" "$www/$i-$(date +%F_%T)" && \cp $i "$www/$i"
-        else
-		[ "$1" == "L" ] &&  echo -e "$www/$i  \033[34m Add File \033[0m"
-		[ "$1" == "yes" ] &&  \cp $i "$www/$i"
-        fi
+for i in $(find $DIR -type f); do
+	if [ -f "$WWW/$i" ]; then
+		[ "$1" == "L" ] && echo -e "$WWW/$i  \033[34m Update File \033[0m"
+		[ "$1" == "yes" ] && cp "$WWW/$i" "$WWW/$i-$(date +%F_%T)" && \cp $i "$WWW/$i"
+#		[ "$1" == "yes" ] && cp "$WWW/$i" "$TIME/$i" && \cp $i "$WWW/$i"
+	else
+		[ "$1" == "L" ] && echo -e "$WWW/$i  \033[32m Add File \033[0m"
+		[ "$1" == "yes" ] &&  \cp $i "$WWW/$i"
+	fi
 done
