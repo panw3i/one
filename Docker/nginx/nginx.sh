81cc9f8ccc58abadd21d543c9bea9d0a97f91611
JIOBXN <jiobxn@gmail.com>
2017-05-02 19:46:58 +0800

    Update nginx.sh

--- Docker/nginx/nginx.sh
+++ Docker/nginx/nginx.sh
@@ -100,6 +100,9 @@ http_conf() {
 	    location / {
 	        root   html;
 	        index  index.html index.htm;
+
+	##user_auth        auth_basic           "Nginx Auth";
+	##user_auth        auth_basic_user_file /etc/nginx/.htpasswd-tag;
 	    }
 
 	##nginx_status    location ~ /basic_status {
