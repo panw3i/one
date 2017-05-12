16bf8cfff22785873b235792d30077ed551866e0
JIOBXN <jiobxn@gmail.com>
2017-05-12 14:19:32 +0800

    Update backup.sh

--- Docker/mysql/backup.sh
+++ Docker/mysql/backup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 DATA=$(mysqld --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')
 PASS=newpass
+PORT=3306
 BDAY=3
 
 [ ! -d "$DATA/mysql_back" ] && mkdir "$DATA/mysql_back"
 
 cd "$DATA/mysql_back"
-for i in $(MYSQL_PWD="$PASS" mysql -uroot -e "show databases;" |awk 'NR!=1{print $1}' |egrep -v "information_schema|performance_schema|mysql|sys"); do
-	MYSQL_PWD="$PASS" /usr/bin/mysqldump -uroot --single-transaction "$i" >"$i"_`date +%F`_db.sql 2>/dev/null
+for i in $(MYSQL_PWD="$PASS" mysql -uroot -p$PORT -e "show databases;" |awk 'NR!=1{print $1}' |egrep -v "information_schema|performance_schema|mysql|sys"); do
+	MYSQL_PWD="$PASS" /usr/bin/mysqldump -uroot -p$PORT --single-transaction "$i" >"$i"_`date +%F`_db.sql 2>/dev/null
 	tar czf "$i"_`date +%F`_db.tar.gz "$i"_`date +%F`_db.sql
 	\rm "$i"_`date +%F`_db.sql
 done
