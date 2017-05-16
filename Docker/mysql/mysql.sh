fa624a81d6587d4c61007aed5630b50691a0e9e0
JIOBXN <jiobxn@gmail.com>
2017-05-16 12:55:40 +0800

    Update mysql.sh

--- Docker/mysql/mysql.sh
+++ Docker/mysql/mysql.sh
@@ -181,12 +181,12 @@ if [ "$1" = 'mysqld_safe' ]; then
 		cat > /iptables.sh <<-END
 		iptables -I INPUT -p tcp --dport $MYSQL_PORT -j DROP
 		iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport $MYSQL_PORT -j ACCEPT
+		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport $MYSQL_PORT -m comment --comment MYSQL -j ACCEPT
 		END
 	fi
 
 	echo "Start MYSQL ****"
-	#[ -f /iptables.sh ] && . /iptables.sh
+	[ -f /iptables.sh ] && [ -z "`iptables -S |grep MYSQL`" ] && . /iptables.sh
 	crond
 
 	exec "$@"
