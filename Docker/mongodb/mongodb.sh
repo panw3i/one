f07c8c6c17f9af70c946f98a37d2e646de5f6682
JIOBXN <jiobxn@gmail.com>
2017-06-04 00:59:14 +0800

    Update mongodb.sh

--- Docker/mongodb/mongodb.sh
+++ Docker/mongodb/mongodb.sh
@@ -24,7 +24,7 @@ if [ "$1" = 'mongod' ]; then
 		mongo <admin.json &>/dev/null
 		echo "MongoDB ROOT PASSWORD: $MONGO_ROOT_PASS" |tee /var/lib/mongo/root_info
 		AUTH="-u root -p $MONGO_ROOT_PASS --authenticationDatabase admin"
-		sed -i 's/PASS=/PASS="'$AUTH'"/' /backup.sh
+		sed -i "s/PASS=/PASS=\"$AUTH\"/" /backup.sh
 	fi
 
 	#Create a database and database user
@@ -48,29 +48,9 @@ if [ "$1" = 'mongod' ]; then
 	}
 
 
-	##BASE
-	mongo_base() {
-	mongod -f /etc/mongod.conf
-	sed -i 's/#security:/security:/' /etc/mongod.conf
-	sed -i '/security:/ a \  authorization: enabled' /etc/mongod.conf
-	sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-	sed -i 's/fork: true/#fork: true/' /etc/mongod.conf
-	[ "$MONGO_HTTP" ] && sed -i '/net:/ a \  http:\n \    enabled: true' /etc/mongod.conf
-	mongo_user
-	/usr/local/bin/mongod -f /etc/mongod.conf --shutdown &>/dev/null
-	}
-
-
 	##Clustered
 	mongo_gluster() {
 	if [ "$VIP" ]; then
-		sed -i 's/#replication:/replication:/' /etc/mongod.conf
-		sed -i '/replication:/ a \  replSetName: '$REPL_NAME'' /etc/mongod.conf
-		sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-		[ "$MONGO_HTTP" ] && sed -i '/net:/ a \  http:\n \    enabled: true' /etc/mongod.conf
-		[ "$MONGO_SERVER" ] && /usr/local/bin/mongod -f /etc/mongod.conf &>/dev/null
-		sed -i 's/fork: true/#fork: true/' /etc/mongod.conf
-
 		cat >/vip.sh<<-END
 		#!/bin/bash
 		PASS="$AUTH"
@@ -98,6 +78,7 @@ if [ "$1" = 'mongod' ]; then
 		END
 		chmod +x /vip.sh
 		echo "* * * * * . /etc/profile;/bin/sh /vip.sh &>/dev/null" >>/var/spool/cron/root
+		\cp /vip.sh /var/lib/mongo/vip.sh
 	fi
 	
 	
@@ -107,23 +88,30 @@ if [ "$1" = 'mongod' ]; then
 		if [ -n "$(ifconfig |grep $i)" ]; then
 			cat >replset.json<<-END
 			rs.initiate( {
-				_id : "$MONGO_ID",
+				_id : "$REPL_NAME",
 				members: [ { _id : 0, host : "$i:27017" } ]
 			})
 			END
 			mongo < replset.json 1>/dev/null
+			echo "$REPL_NAME" >/var/lib/mongo/repl_info
 		fi
 		done
+
 		for i in $(echo $MONGO_SERVER |sed 's/,/\n/g'); do
-		[ -z "$(ifconfig |grep $i)" ] && echo "rs.add(\"$i\")" |mongo 1>/dev/null
+			[ -z "$(ifconfig |grep $i)" ] && echo "rs.add(\"$i\")" |mongo 1>/dev/null
 		done
+
 		mongo_user
+		echo "Start MongoDB ****"
 		exec "/usr/sbin/init"
 	fi
 	}
 
 
+	##Other
 	mongo_other() {
+	echo "Initializing MongoDB"
+	
 	#Backup Database
 	if [ "$MONGO_BACK" ]; then
 		echo "0 4 * * * . /etc/profile;/bin/sh /backup.sh &>/dev/null" >>/var/spool/cron/root
@@ -139,29 +127,61 @@ if [ "$1" = 'mongod' ]; then
 		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport 28017 -m comment --comment MONGDB -j ACCEPT
 		END
 	fi
-	
-	[ -f /iptables.sh ] && [ -z "`iptables -S |grep MONGDB`" ] && . /iptables.sh || echo
-	crond
 	}
 
 
-	##start
+	##Start Configuration
 	if [ -d "/var/lib/mongo/diagnostic.data" ]; then
 		echo "/var/lib/mongo/diagnostic.data already exists, skip"
-		mongo_other
+		if [ -z "$(grep "redhat.xyz" /etc/mongod.conf)" ]; then
+			sed -i '1 i #redhat.xyz' /etc/mongod.conf
+			sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+			sed -i 's/fork: true/#fork: true/' /etc/mongod.conf
+			[ "$MONGO_HTTP" ] && sed -i '/net:/ a \  http:\n \    enabled: true' /etc/mongod.conf
+
+			[ -z "$REPL_NAME" ] && [ -f "/var/lib/mongo/repl_info" ] && REPL_NAME=$(cat /var/lib/mongo/repl_info)
+			if [ "$REPL_NAME" ]; then
+				sed -i 's/#replication:/replication:/' /etc/mongod.conf
+				sed -i '/replication:/ a \  replSetName: '$REPL_NAME'' /etc/mongod.conf
+			fi 
+
+			[ -f "/var/lib/mongo/vip.sh" ] && \cp /var/lib/mongo/vip.sh /vip.sh && echo "* * * * * . /etc/profile;/bin/sh /vip.sh &>/dev/null" >>/var/spool/cron/root
+		
+			mongo_other
+		fi
 	else
-		echo "Initializing MongoDB"
 		if [ "$VIP" ]; then
 			mongo_other
+
+			sed -i '1 i #redhat.xyz' /etc/mongod.conf
+			sed -i 's/#replication:/replication:/' /etc/mongod.conf
+			sed -i '/replication:/ a \  replSetName: '$REPL_NAME'' /etc/mongod.conf
+			sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+			[ "$MONGO_HTTP" ] && sed -i '/net:/ a \  http:\n \    enabled: true' /etc/mongod.conf
+			[ "$MONGO_SERVER" ] && /usr/local/bin/mongod -f /etc/mongod.conf &>/dev/null
+			sed -i 's/fork: true/#fork: true/' /etc/mongod.conf
+
 			mongo_gluster
 		else
-			mongo_base
 			mongo_other
+
+			mongod -f /etc/mongod.conf 1>/dev/null
+			sed -i '1 i #redhat.xyz' /etc/mongod.conf
+			sed -i 's/#security:/security:/' /etc/mongod.conf
+			sed -i '/security:/ a \  authorization: enabled' /etc/mongod.conf
+			sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+			sed -i 's/fork: true/#fork: true/' /etc/mongod.conf
+			[ "$MONGO_HTTP" ] && sed -i '/net:/ a \  http:\n \    enabled: true' /etc/mongod.conf
+
+			mongo_user
+			/usr/local/bin/mongod -f /etc/mongod.conf --shutdown &>/dev/null
 		fi
 	fi
 
 
 	echo "Start MongoDB ****"
+	[ -f /iptables.sh ] && [ -z "`iptables -S |grep MONGDB`" ] && . /iptables.sh || echo
+	crond
 	exec "$@" 1>/dev/null
 
 else
