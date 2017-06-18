2f8677ce87a9a1c36ff4e9d539e5c4cf81c682dc
JIOBXN <jiobxn@gmail.com>
2017-06-18 14:49:59 +0800

    Update ssserver.sh

--- Docker/shadowsocks/ssserver.sh
+++ Docker/shadowsocks/ssserver.sh
@@ -14,18 +14,18 @@ if [ "$1" = 'shadowsocks' ]; then
 if [ ! -f /usr/bin/shadowsocks ]; then 
 	if [ $SS_ADDR ]; then
 		if [ $SSR ]; then
-			echo -e "shadowsocks type: SSR\nlocal port: $SL_PORT"
+			echo -e "shadowsocks type: SSR \nlocal port: $SL_PORT"
 			echo "/shadowsocksr/shadowsocks/local.py -s $SS_ADDR -p $SS_PORT -k $SS_PASS -m $SS_EMOD -O $SS_PROTOCOL -o $SS_OBFS -b $SL_ADDR -l $SL_PORT" >/usr/bin/shadowsocks
 		else
-			echo -e "shadowsocks type: SS\nlocal port: $SL_PORT"
+			echo -e "shadowsocks type: SS \nlocal port: $SL_PORT"
 			echo "/shadowsocks/shadowsocks/local.py -s $SS_ADDR -p $SS_PORT -k $SS_PASS -m $SS_EMOD -b $SL_ADDR -l $SL_PORT" >/usr/bin/shadowsocks
 		fi
 	else
 		if [ $SSR ]; then
-			echo -e "shadowsocks type: SSR\npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT"
+			echo -e "shadowsocks type: SSR \npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT \nprotocol: $SS_PROTOCOL \nobfs: $SS_OBFS"
 			echo "/shadowsocksr/shadowsocks/server.py -p $SS_PORT -k $SS_PASS -m $SS_EMOD -O $SS_PROTOCOL -o $SS_OBFS" >/usr/bin/shadowsocks
 		else
-			echo -e "shadowsocks type: SS\npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT"
+			echo -e "shadowsocks type: SS \npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT"
 			echo "/shadowsocks/shadowsocks/server.py -p $SS_PORT -k $SS_PASS -m $SS_EMOD" >/usr/bin/shadowsocks
 		fi
 	fi
