fb847010d7614698a2a74de84f74e2da194a7e0d
JIOBXN <jiobxn@gmail.com>
2017-06-30 16:24:15 +0800

    Update show_veth.sh

--- Script/show_veth.sh
+++ Script/show_veth.sh
@@ -3,7 +3,7 @@
 for container in $(docker ps |awk 'NR!=1{print $NF}'); do
     iflink=`docker exec -it $container bash -c 'cat /sys/class/net/eth0/iflink'`
     iflink=`echo $iflink|tr -d '\r'`
-    veth=`grep -l $iflink /sys/class/net/veth*/ifindex`
+    veth=`grep -l $iflink /sys/class/net/veth*/ifindex 2>/dev/null`
     veth=`echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
     echo $container:$veth
 done
