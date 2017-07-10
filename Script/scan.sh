f820f8aeb5a278a61f13fd6f593820409860bd01
JIOBXN <jiobxn@gmail.com>
2017-07-10 14:11:44 +0800

    Update scan.sh

--- Script/scan.sh
+++ Script/scan.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 SSH=$(egrep "Failed" /var/log/secure |awk '{print $(NF-3)}' |grep ^[1-9] |sort |uniq -c |awk '$1>10' |awk '{print $1"="$2}')  
+#SSH=$(lastb |awk '$3~"^[1-9]"{print $3}' |sort |uniq -c |awk '$1>10' |awk '{print $1"="$2}')
 
 for i in $SSH
 do  
@@ -7,8 +8,7 @@ do
     SCANIP=`echo $i |awk -F= '{print $2}'`   
     echo "$SCANIP($NUMBER)"  
 
-    if [ -z "`/sbin/iptables -vnL INPUT | grep $SCANIP`" ]   
-    then   
+    if [ -z "`/sbin/iptables -vnL INPUT | grep $SCANIP`" ]; then   
         /sbin/iptables -I INPUT -s $SCANIP -j DROP
         ADDRESS=`/usr/bin/curl -s "http://ip138.com/ips138.asp?ip=$SCANIP&action=2"| iconv -f gb2312 -t utf-8|grep '<ul class="ul1"><li>' | awk -F '[<> ]+' '{print $7,$8}' | awk -F： '{print $2}'`
         echo "`date +%-F/%-H:%-M:%-S` $SCANIP($NUMBER) $ADDRESS" >> /var/log/scanip.log
