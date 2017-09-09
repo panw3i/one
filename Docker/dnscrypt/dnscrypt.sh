920fc3a304e42793d117d3155c3d38e0b2dadabd
JIOBXN <jiobxn@users.noreply.github.com>
2017-09-09 22:05:46 +0800

    Update dnscrypt.sh

--- Docker/dnscrypt/dnscrypt.sh
+++ Docker/dnscrypt/dnscrypt.sh
@@ -4,7 +4,6 @@ set -e
 if [ "$1" = 'dnscrypt' ]; then
 
 : ${CERT_DAY:="365"}
-: ${SERVER_DOMAIN:="jiobxn.com"}
 : ${SERVER_LISTEN:="0.0.0.0:5443"}
 : ${CLIENT_LISTEN:="0.0.0.0:53"}
 : ${BIND_VERSION:="windows 2003 DNS"}
@@ -20,22 +19,32 @@ if [ ! -f /usr/bin/dnscrypt ]; then
 		sed -i '/#jiobxn.com#/ a \        version  "'"$BIND_VERSION"'";' /etc/named.conf
 		sed -i 's/127.0.0.1;/'$BIND_LISTEN'/' /etc/named.conf
 		sed -i 's/localhost;/'$BIND_ALLOW_QUERY'/' /etc/named.conf
-		
-		[ "$BIND_DISABLE_RECURSION" ] && sed -i 's/recursion yes;/recursion no;/' /etc/named.conf
-		[ "$BIND_FORWARDERS" ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$BIND_FORWARDERS' };' /etc/named.conf
-		[ $BIND_FORWARD_ONLY ] && sed -i '/#jiobxn.com#/ a \\n\        forward only;' /etc/named.conf
+
+		[ $BIND_FORWARDERS ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$BIND_FORWARDERS' };' /etc/named.conf
 		[ $BIND_CACHE_SIZE ] && sed -i '/#jiobxn.com#/ a \\n\        max-cache-size '$BIND_CACHE_SIZE';' /etc/named.conf
-		[ $BIND_ALLOW_RECURSION ] && sed -i '/#jiobxn.com#/ a \\n\        allow-recursion { '$BIND_ALLOW_RECURSION' };' /etc/named.conf
 		
-		if [ $BIND_ENABLE_LOG ]; then
+		if [ $BIND_QUERY_LOG ]; then
 			sed -i '/logging {/ a \        channel query_log {\n\            file "data/query.log" versions 3 size '$BIND_LOG_SIZE';\n\            severity info;\n\            print-time yes;\n\            print-category   yes;\n\        };' /etc/named.conf
 			sed -i '/logging {/ a \        category queries {\n\            query_log;\n\        };' /etc/named.conf
 		fi
 	}
 
+	#Cisco OpenDNS
+	if [ "$CLIENT_UPSTREAM" == "CISCO" ]; then
+		SERVER_DOMAIN="opendns.com"
+		CLIENT_UPSTREAM="208.67.220.220"
+		PROVIDER_KEY="B735:1140:206F:225D:3E2B:D822:D7FD:691E:A1C3:3CC8:D666:8D0C:BE04:BFAB:CA43:FB79"
+	fi
+
+	#Cisco OpenDNS,Block websites not suitable for children
+	if [ "$CLIENT_UPSTREAM" == "HOME" ]; then
+		SERVER_DOMAIN="opendns.com"
+		CLIENT_UPSTREAM="208.67.220.123"
+		PROVIDER_KEY="B735:1140:206F:225D:3E2B:D822:D7FD:691E:A1C3:3CC8:D666:8D0C:BE04:BFAB:CA43:FB79"
+	fi
 
 	#server
-	if [ $SERVER_UPSTREAM ]; then
+	if [ "$SERVER_UPSTREAM" -a "$SERVER_DOMAIN" ]; then
 		cd /key/
 		if [ ! -f /key/dnscrypt.key -a ! -f /key/dnscrypt.cert -a ! -f /key/public.key ]; then
 			dnscrypt-wrapper --gen-provider-keypair &>/dev/null
@@ -49,12 +58,12 @@ if [ ! -f /usr/bin/dnscrypt ]; then
 		echo "Public server: https://github.com/jedisct1/dnscrypt-proxy/blob/master/dnscrypt-resolvers.csv" |tee -a dnscrypt.log
 		\cp /usr/local/share/dnscrypt-proxy/dnscrypt-resolvers.csv .
 
-		echo "dnscrypt-wrapper --resolver-address=$SERVER_UPSTREAM --listen-address=$SERVER_LISTEN --provider-name=2.dnscrypt-cert.$SERVER_DOMAIN --crypt-secretkey-file=dnscrypt.key --provider-cert-file=dnscrypt.cert" >/usr/bin/dnscrypt
+		echo "/usr/sbin/named -u named -c /etc/named.conf" >/usr/bin/dnscrypt
+		echo "dnscrypt-wrapper --resolver-address=$SERVER_UPSTREAM --listen-address=$SERVER_LISTEN --provider-name=2.dnscrypt-cert.$SERVER_DOMAIN --crypt-secretkey-file=dnscrypt.key --provider-cert-file=dnscrypt.cert" >>/usr/bin/dnscrypt
 	fi
 
 	#client
-	if [ "$CLIENT_UPSTREAM" -a "$PROVIDER_KEY" ]; then
-		BIND_FORWARDERS="$(echo $CLIENT_LISTEN |sed 's/0.0.0.0/127.0.0.1/' |awk -F: '{print $1}');"
+	if [ "$CLIENT_UPSTREAM" -a "$PROVIDER_KEY" -a "$SERVER_DOMAIN" ]; then
 		echo "dnscrypt-proxy --local-address=$CLIENT_LISTEN --resolver-address=$CLIENT_UPSTREAM --provider-name=2.dnscrypt-cert.$SERVER_DOMAIN --provider-key=$PROVIDER_KEY" >/usr/bin/dnscrypt
 	fi
 
@@ -63,9 +72,17 @@ if [ ! -f /usr/bin/dnscrypt ]; then
 		echo "/usr/sbin/named -u named -c /etc/named.conf -f" >/usr/bin/dnscrypt
 	fi
 
-	#bind
-	if [ -n "$SERVER_UPSTREAM" ]; then
-		sed -i '1i /usr/sbin/named -u named -c /etc/named.conf' /usr/bin/dnscrypt
+	#chinadns
+	if [ "$CHINADNS" -a "$CLIENT_UPSTREAM" -a "$PROVIDER_KEY" -a "$SERVER_DOMAIN" ]; then
+		echo "/usr/sbin/named -u named -c /etc/named.conf" >/usr/bin/dnscrypt
+		echo "dnscrypt-proxy --local-address=0.0.0.0:54 --resolver-address=$CLIENT_UPSTREAM --provider-name=2.dnscrypt-cert.$SERVER_DOMAIN --provider-key=$PROVIDER_KEY -d" >>/usr/bin/dnscrypt
+		echo "chinadns -c /key/chnroute.txt -b 0.0.0.0 -p 53 -s '127.0.0.1:54,127.0.0.1:55' -d -v" >>/usr/bin/dnscrypt
+
+		sed -i 's/port 53/port 55/g' /etc/named.conf
+
+		if [ ! -f /key/chnroute.txt ]; then
+			grep "CN|ipv4" /etc/delegated-apnic-latest |awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' >/key/chnroute.txt
+		fi
 	fi
 
 	init_bind
@@ -79,27 +96,23 @@ else
 	Example:
 				docker run -d \\
 				-v /docker/dnscrypt:/key \\
-				-v /docker/bind_log:/var/named/data \\
-				-p 5443:5443 \\
 				-p 5443:5443/udp \\
 				-p 53:53/udp \\
 				-e CERT_DAY=[365] \\
-				-e SERVER_DOMAIN=[jiobxn.com] \\
+				-e SERVER_DOMAIN=<jiobxn.com> \\
 				-e SERVER_LISTEN=[0.0.0.0:5443] \\
 				-e SERVER_UPSTREAM=<8.8.8.8:53> \\
 				-e CLIENT_LISTEN=[0.0.0.0:53] \\
-				-e CLIENT_UPSTREAM=<server address> \\
+				-e CLIENT_UPSTREAM=<"server address" | CISCO | HOME> \\
 				-e PROVIDER_KEY=<Provider public key>
+				-e CHINADNS=<Y> \\
 				-e BIND_VERSION=["windows 2003 DNS"] \\
 				-e BIND_LOG_SIZE=[100m] \\
-				-e BIND_LISTEN=["127.0.0.1;"] \\
+				-e BIND_LISTEN=["any;"] \\
 				-e BIND_ALLOW_QUERY=["any;"] \\
-				-e BIND_DISABLE_RECURSION=<Y> \\
-				-e BIND_FORWARDERS=<12.0.0.1> \\
-				-e BIND_FORWARD_ONLY=<Y> \\
+				-e BIND_FORWARDERS=<8.8.8.8;> \\
 				-e BIND_CACHE_SIZE=[32m] \\
-				-e BIND_ALLOW_RECURSION=["any;"] \\
-				-e BIND_ENABLE_LOG=<Y> \\
+				-e BIND_QUERY_LOG=<Y> \\
 				--hostname dns --name dns dnscrypt
 	"
 fi
