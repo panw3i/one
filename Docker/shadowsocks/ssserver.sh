798253da72d9960f73823a8b89894bbdd8ccde25
JIOBXN <jiobxn@users.noreply.github.com>
2017-09-11 17:59:19 +0800

    Update ssserver.sh

--- Docker/shadowsocks/ssserver.sh
+++ Docker/shadowsocks/ssserver.sh
@@ -3,30 +3,30 @@ set -e
 
 if [ "$1" = 'shadowsocks' ]; then
 
-: ${SS_PASS:="$(openssl passwd $RANDOM)"}
-: ${SS_EMOD:="aes-256-cfb"}
-: ${SS_PORT:="8443"}
-: ${SS_OBFS:="tls1.2_ticket_auth_compatible"}
-: ${SS_PROTOCOL:="auth_aes128_sha1"}
-: ${SL_ADDR:="127.0.0.1"}
-: ${SL_PORT:="1080"}
+: ${SS_K:="$(openssl passwd $RANDOM)"}
+: ${SS_M:="aes-256-cfb"}
+: ${SS_P:="8443"}
+: ${SS_o:="tls1.2_ticket_auth_compatible"}
+: ${SS_O:="auth_aes128_sha1"}
+: ${SS_B:="127.0.0.1"}
+: ${SS_L:="1080"}
 
 if [ ! -f /usr/bin/shadowsocks ]; then 
-	if [ $SS_ADDR ]; then
+	if [ $SS_S ]; then
 		if [ $SSR ]; then
-			echo -e "shadowsocks type: SSR \nlocal port: $SL_PORT"
-			echo "/shadowsocksr/shadowsocks/local.py -s $SS_ADDR -p $SS_PORT -k $SS_PASS -m $SS_EMOD -O $SS_PROTOCOL -o $SS_OBFS -b $SL_ADDR -l $SL_PORT" >/usr/bin/shadowsocks
+			echo -e "shadowsocks type: SSR \nlocal port: $SS_L"
+			echo "/shadowsocksr/shadowsocks/local.py -s $SS_S -p $SS_P -k $SS_K -m $SS_M -O $SS_O -o $SS_o -b $SS_B -l $SS_L" >/usr/bin/shadowsocks
 		else
-			echo -e "shadowsocks type: SS \nlocal port: $SL_PORT"
-			echo "/shadowsocks/shadowsocks/local.py -s $SS_ADDR -p $SS_PORT -k $SS_PASS -m $SS_EMOD -b $SL_ADDR -l $SL_PORT" >/usr/bin/shadowsocks
+			echo -e "shadowsocks type: SS \nlocal port: $SS_L"
+			echo "/shadowsocks/shadowsocks/local.py -s $SS_S -p $SS_P -k $SS_K -m $SS_M -b $SS_B -l $SS_L" >/usr/bin/shadowsocks
 		fi
 	else
 		if [ $SSR ]; then
-			echo -e "shadowsocks type: SSR \npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT \nprotocol: $SS_PROTOCOL \nobfs: $SS_OBFS"
-			echo "/shadowsocksr/shadowsocks/server.py -p $SS_PORT -k $SS_PASS -m $SS_EMOD -O $SS_PROTOCOL -o $SS_OBFS" >/usr/bin/shadowsocks
+			echo -e "shadowsocks type: SSR \npassword: $SS_K \nencryption mode: $SS_M \nservice port: $SS_P \nprotocol: $SS_O \nobfs: $SS_o"
+			echo "/shadowsocksr/shadowsocks/server.py -p $SS_P -k $SS_K -m $SS_M -O $SS_O -o $SS_o" >/usr/bin/shadowsocks
 		else
-			echo -e "shadowsocks type: SS \npassword: $SS_PASS \nencryption mode: $SS_EMOD \nservice port: $SS_PORT"
-			echo "/shadowsocks/shadowsocks/server.py -p $SS_PORT -k $SS_PASS -m $SS_EMOD" >/usr/bin/shadowsocks
+			echo -e "shadowsocks type: SS \npassword: $SS_K \nencryption mode: $SS_M \nservice port: $SS_P"
+			echo "/shadowsocks/shadowsocks/server.py -p $SS_P -k $SS_K -m $SS_M" >/usr/bin/shadowsocks
 		fi
 	fi
 	chmod +x /usr/bin/shadowsocks 
@@ -39,15 +39,15 @@ else
 	Example:
 				docker run -d --restart always \\
 				-p 8443:8443 \
-				-e SS_PASS=[pwmake 64] \\
-				-e SS_EMOD=[aes-256-cfb] \\
-				-e SS_PORT=[8443] \\
-				-e SS_OBFS=[tls1.2_ticket_auth_compatible] \\
-				-e SS_PROTOCOL=[auth_aes128_sha1] \\
+				-e SS_K=[pwmake 64] \\
+				-e SS_M=[aes-256-cfb] \\
+				-e SS_P=[8443] \\
+				-e SS_o=[tls1.2_ticket_auth_compatible] \\
+				-e SS_O=[auth_aes128_sha1] \\
 				-e SSR=<Y> \\
-				-e SS_ADDR=<SS_ADDR> \\
-				-e SL_ADDR=[127.0.0.1] \\
-				-e SL_PORT=[1080] \\
+				-e SS_S=<SS_SERVER> \\
+				-e SS_B=[127.0.0.1] \\
+				-e SS_L=[1080] \\
 				--hostname shadowsocks --name shadowsocks shadowsocks
 	"
 fi
